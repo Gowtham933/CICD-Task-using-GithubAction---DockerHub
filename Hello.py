125d0dba983055c6119743c8417ad12858b120bf
Gowtham Raj Nallathambi <143486094+Gowtham933@users.noreply.github.com>
2025-02-13 10:04:05 +0100

    Created using Colab

---
+++ Hello.py
@@ -0,0 +1,62 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPrktrETH/j1NxbInSJ4syZ",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Gowtham933/CICD-Task-using-GithubAction---DockerHub/blob/main/Hello.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "n7uG-qXPWFxn",
+        "outputId": "7ba5138d-75cb-47c3-f76d-b7a8676a5f0f"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "my_first CI/CD\n"
+          ]
+        }
+      ],
+      "source": [
+        "print(\"my_first CI/CD\")"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "WPKUPXLUWMGx"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
